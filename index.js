2d08213fb697f3b3e0d40ad858a2dad9f2581434
Quan Bao <quy123bao20@gmail.com>
2020-07-29 09:15:30 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 8080;
+app.set("views", "./views");
+app.set("view engine", "pug");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology:true,
+  useNewUrlParser:true
+},() => {
+  console.log("connected to db")
+});
+
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors")
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+
+app.listen(port, () => console.log(`Example app listening on port ${port} !`));
